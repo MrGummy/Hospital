1191c80b9ad5a3b53908cf42291da098a1952c18
Rodion <kontrolyor@inbox.ru>
2017-11-25 19:29:54 +0600

    Patient + Appointment - constructor, get, set, hashCode, equals, toString

--- Hospital/src/java/model/Appointment.java
+++ Hospital/src/java/model/Appointment.java
@@ -5,10 +5,98 @@
  */
 package model;
 
+import java.util.Date;
+import java.util.Objects;
+
 /**
  *
  * @author Glover
  */
 public class Appointment {
     private int id;
+    private Patient patient;
+    private Doctor doctor;
+    private Date date;
+
+    public Appointment(int id, Patient patient, Doctor doctor, Date date) {
+        this.id = id;
+        this.patient = patient;
+        this.doctor = doctor;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + this.id;
+        hash = 29 * hash + Objects.hashCode(this.patient);
+        hash = 29 * hash + Objects.hashCode(this.doctor);
+        hash = 29 * hash + Objects.hashCode(this.date);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Appointment other = (Appointment) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.patient, other.patient)) {
+            return false;
+        }
+        if (!Objects.equals(this.doctor, other.doctor)) {
+            return false;
+        }
+        if (!Objects.equals(this.date, other.date)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Appointment{" + "id=" + id + ", patient=" + patient + ", doctor=" + doctor + ", date=" + date + '}';
+    }
+    
 }
--- Hospital/src/java/model/Patient.java
+++ Hospital/src/java/model/Patient.java
@@ -5,10 +5,84 @@
  */
 package model;
 
+import java.util.Date;
+import java.util.Objects;
+
 /**
  *
  * @author Glover
  */
-public class Patient {
+public class Patient 
+{
+    private int id;
+    private String fio;
+    private Date birth_date;
     
+    public Patient(int id, String fio, Date birth_date) {
+        this.id = id;
+        this.fio = fio;
+        this.birth_date = birth_date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public Date getBirth_date() {
+        return birth_date;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public void setBirth_date(Date birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + this.id;
+        hash = 59 * hash + Objects.hashCode(this.fio);
+        hash = 59 * hash + Objects.hashCode(this.birth_date);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Patient other = (Patient) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.fio, other.fio)) {
+            return false;
+        }
+        if (!Objects.equals(this.birth_date, other.birth_date)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" + "id=" + id + ", fio=" + fio + ", birth_date=" + birth_date + '}';
+    }
 }
