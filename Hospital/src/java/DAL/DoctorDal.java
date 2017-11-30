0feae25d4c26638bfe5fdb34e2b7fed93a2d723d
Rodion <kontrolyor@inbox.ru>
2017-11-30 19:07:57 +0600

    DoctorDal

--- Hospital/src/java/DAL/DoctorDal.java
+++ Hospital/src/java/DAL/DoctorDal.java
@@ -5,6 +5,9 @@
  */
 package DAL;
 
+import java.util.List;
+import hosp.model.Doctor;
+import org.apache.ibatis.session.SqlSession;
 /**
  *
  * @author Евгений Мартыненко General-zver
@@ -14,5 +17,55 @@ public class DoctorDal extends BaseDal {
     public DoctorDal() {
         super();
     }
+    public List<Doctor> selectAll()
+    {
+         SqlSession session = sqlSessionFactory.openSession(); 
+         List<Doctor> list = session.selectList("doctor.selectAll");
+         session.close();
+         return list;
+    }
     
+    public List<Doctor> selectBySpecialization()
+    {
+         SqlSession session = sqlSessionFactory.openSession(); 
+         List<Doctor> list = session.selectList("doctor.selectBySpecialization");
+         session.close();
+         return list;
+    }
+    
+    public Doctor selectById(int id)
+    {
+         SqlSession session = sqlSessionFactory.openSession(); 
+         Doctor doctor= session.selectOne("doctor.selectById",id);
+         session.close();
+         return doctor;
+    }
+    
+    public int update(Doctor doctor)
+    {
+         SqlSession session = sqlSessionFactory.openSession(); 
+         int count= session.update("doctor.update",doctor);
+         session.commit();
+         session.close();
+         return count;
+    }
+    
+    public int insert(Doctor doctor)
+    {
+         SqlSession session = sqlSessionFactory.openSession(); 
+         int count= session.insert("doctor.insert",doctor);
+         session.commit();
+         session.close();
+         return count;
+    }
+     
+    public int delete(int id)
+    {
+         SqlSession session = sqlSessionFactory.openSession(); 
+         int count= session.delete("doctor.deleteById",id);
+         session.commit();
+         session.close();
+         return count;
+    }
 }
+
