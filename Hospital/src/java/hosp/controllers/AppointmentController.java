4eaf603b7fddc58f1629d1fb5b8c1a5887626dd0
General-zver <general_gon@mail.ru>
2017-11-30 19:48:10 +0600

    +AppointmentController

--- Hospital/src/java/hosp/controllers/AppointmentController.java
+++ Hospital/src/java/hosp/controllers/AppointmentController.java
@@ -5,10 +5,44 @@
  */
 package hosp.controllers;
 
+import java.util.List;
+import hosp.DAL.AppointmentDal;
+import hosp.model.Appointment;
+
 /**
  *
- * @author Glover
+ * @author General-zver
  */
 public class AppointmentController {
+    protected AppointmentDal appointmentDal;
+    public AppointmentController() 
+    {
+        appointmentDal = new AppointmentDal();
+    }
     
+    public List<Appointment> getAllAppointment()
+    {
+        return appointmentDal.selectAll();
+    }
+    
+    public Appointment getAppointment(int id)
+    {
+        return appointmentDal.selectById(id);
+    }
+     
+    public int insertAppointment(Appointment appointment)
+    {
+        return appointmentDal.insert(appointment);
+    }
+      
+    public int updateAppointment(Appointment appointment)
+    {
+        return appointmentDal.update(appointment);
+    }
+    
+    public int deleteAppointmentById(int id)
+    {
+        return appointmentDal.delete(id);
+    }
+            
 }
