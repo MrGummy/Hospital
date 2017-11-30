04a98f2ff258e5203dee007dc544b3a63817af4b
General-zver <general_gon@mail.ru>
2017-11-30 20:00:25 +0600

    update Json Doctor/Patient

--- Hospital/src/java/hosp/mappers/JsonDoctorMapper.java
+++ Hospital/src/java/hosp/mappers/JsonDoctorMapper.java
@@ -5,10 +5,51 @@
  */
 package hosp.mappers;
 
+import hosp.model.Doctor;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.codehaus.jackson.map.ObjectMapper;
+
 /**
  *
- * @author Евгений Мартыненко L
+ * @author Евгений Мартыненко General-zver
  */
 public class JsonDoctorMapper {
     
+    public static String toJSON(Doctor doctor)
+    {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String json=mapper.writeValueAsString(doctor);
+            return json;
+        } catch (IOException ex) {
+            Logger.getLogger(JsonDoctorMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+     public static String toJSON(List<Doctor> doctor)  {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String json=mapper.writeValueAsString(doctor);
+            return json;
+        } catch (IOException ex) {
+            Logger.getLogger(JsonDoctorMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    
+    public static Doctor fromJSON(String json)  {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Doctor doctor =mapper.readValue(json, Doctor.class);
+            return doctor;
+        } catch (IOException ex) {
+            Logger.getLogger(JsonDoctorMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
 }
--- Hospital/src/java/hosp/mappers/JsonPatientMapper.java
+++ Hospital/src/java/hosp/mappers/JsonPatientMapper.java
@@ -5,10 +5,50 @@
  */
 package hosp.mappers;
 
+import hosp.model.Patient;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.codehaus.jackson.map.ObjectMapper;
+
 /**
  *
- * @author Евгений Мартыненко L
+ * @author Евгений Мартыненко General-zver
  */
 public class JsonPatientMapper {
+    public static String toJSON(Patient patient)
+    {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String json=mapper.writeValueAsString(patient);
+            return json;
+        } catch (IOException ex) {
+            Logger.getLogger(JsonPatientMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
     
+     public static String toJSON(List<Patient> patient)  {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String json=mapper.writeValueAsString(patient);
+            return json;
+        } catch (IOException ex) {
+            Logger.getLogger(JsonPatientMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    
+    public static Patient fromJSON(String json)  {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Patient patient =mapper.readValue(json, Patient.class);
+            return patient;
+        } catch (IOException ex) {
+            Logger.getLogger(JsonPatientMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
 }
