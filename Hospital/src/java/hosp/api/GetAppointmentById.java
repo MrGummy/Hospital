a7f67efe728830f350ae93aa1988a2298e33671a
Rodion <kontrolyor@inbox.ru>
2017-11-30 20:39:47 +0600

    GetPatientById + GetDoctorById + GetAppointmentById + InsertPatient + InsertDoctor

--- Hospital/src/java/hosp/api/GetAppointmentById.java
+++ Hospital/src/java/hosp/api/GetAppointmentById.java
@@ -5,10 +5,84 @@
  */
 package hosp.api;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import hosp.controllers.AppointmentController;
+import hosp.mappers.JsonAppointmentMapper;
+import hosp.model.Appointment;
+
 /**
  *
  * @author Glover
  */
-public class GetAppointmentById {
-    
+@WebServlet(name = "GetAppointmentById", urlPatterns = {"/GetAppointmentById"})
+public class GetAppointmentById extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        int id = Integer.parseInt(request.getParameter("id"));
+        try (PrintWriter out = response.getWriter()) 
+        {
+             AppointmentController appointmentController = new AppointmentController();
+             Appointment appointment= appointmentController.getAppointment(id);
+             String json=JsonAppointmentMapper.toJSON(appointment);
+             out.println(json);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
 }
